1b7ae5f7e7e67b16f0562727838266e66e9c1b1e
Juan Paulo Lara Rodríguez <jplarar@gmail.com>
2015-11-21 01:45:51 -0600

    update script

--- update.sh
+++ update.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
+
+echo --- Pull the most recent configuration files ---
+git stash
+git pull
+if [ $? -gt 0 ]; then
+    chmod +x update.sh
+    echo "Update stopped. Could not pull from remote repository."
+else
+
 echo - Update packages with composer -
 php composer.phar self-update
  php composer.phar update # bug
 
 echo - Fix permissions -
- chmod 777 -R app/logs
+ chmod 755 -R app/logs
 
 echo - Clear cache -
- chmod 777 -R app/cache
+ chmod 755 -R app/cache
  php app/console cache:clear --env=prod # bug
- chmod 777 -R app/cache
+ chmod 755 -R app/cache
 
 echo - Update database -
 php app/console doctrine:schema:update --force
+
+fi
